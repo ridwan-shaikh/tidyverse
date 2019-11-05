d4ccee76a186378e36f82a2efe8c93b3f6451f1d
Ridwan Shaikh <ridwanshaikh@11089mopdt.icr.ac.uk>
2019-11-05 16:57:13 +0000

    final

--- dplyr.R
+++ dplyr.R
@@ -14,9 +14,8 @@
 install.packages("tidyverse")
 
 #let get a dataset to play around with...
-#pasilla dataset from Brooks et al. Investigation into the effect of siRNA knock-down of pasilla,
-#a gene that is thought to be involved in regulation of splicing. The dataset contains 3 biological replicates
-#of the knockdown and 4 biological replicates for the untreated control.
+#this is just a summary of the actual pasilla dataset. I wanted to keep it 
+#small so its easier to visualise
 BiocManager::install("pasilla")
 
 #find location of the data
@@ -27,23 +26,23 @@ pasilla <- read.delim(file.path(location_of_dataset, "pasilla_sample_annotation.
 
 #load the library
 #we will use require in this instance just to illustrate the packages that are attached
-#feel free to use the normal library()
+#feel free to use the normal library() in every other case
 require(tidyverse)
 
-# rules for tidyverse
-# each variable in in its own column
-# each observation/case is in its own row
+# rules for tidyverse:
+# - each variable in in its own column
+# - each observation/case is in its own row
 
-#it also introduced the concenpt of pipeing something into something else
+#it also introduces the concenpt of piping something into something else
 #if you have ever used command line it is akin to "|"
-#but in R it is represented as %>%
+#but in tidyverse it is represented as %>%
 
 #Lets find out the total number of lanes we have use throughout the experiment
 summarise(pasilla, total = sum(exon.counts))
 
 #or with the pipe we could do this: 
 #(notice how when you pipe, you do not need to use the $ operator to call a vector. 
-#Autofill with tab should also show the possible vectors)
+#Autofill with tab should also show the possible colnames)
 pasilla %>%
   summarise(total = sum(exon.counts))
 
@@ -58,7 +57,6 @@ pasilla %>%
   filter(condition == "treated") %>%
   filter(exon.counts < 14e6)
 
-
 #lets calculate reads per lane. To create new columns, dplyr has a function called mutate.
 #mutate allows you to apply vectorised functions to columns
 #notice the total number of reads is x2 for paired end reads. Thats not helpful is it?
@@ -67,13 +65,33 @@ pasilla %>%
 pasilla %>%
   mutate(reads.per.lane = as.numeric(stringr::str_extract(total.number.of.reads, "[0-9]+")) / number.of.lanes)
 
+#where is really becomes helpful is when you want to combine two sets of dataframes
+#lets create a new dataframe using the filenames from the original pasilla dataframe
+#I added some NA in the data just to show you what it can do with the different types of join
+
+pasilla2 <- data.frame(
+  file = as.factor(c(paste(pasilla$file), "untreated5fb")),
+  GeneA = rnorm(8, 30, 30),
+  GeneB = c(rnorm(4, 100, 10), NA, (rnorm(3, 100, 10))),
+  GeneC = c(rnorm(7, 60, 5), NA),
+  GeneD = c(rnorm(6, 40, 60), NA, NA)
+)
 
 
+#lets join the dataframe together using full join
+pasilla %>%
+  full_join(pasilla2, by = "file")
 
+#maybe the syntax for this is easier when you do not pipe
+right_joined_pasilla <- right_join(pasilla, pasilla2, by = "file")
 
+left_joined_pasilla <- left_join(pasilla, pasilla2, by = "file")
 
+#if they do not have the same header name?
+#you can join then using named vectors using the c()
 
-
+pasilla %>%
+  full_join(pasilla2, by = c("file", "file"))
 
 
 
